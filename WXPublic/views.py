404128905e4ba4bf0ea98638e09ba9367b9d0b5a
曾志强 <749970817@qq.com>
2023-03-07 14:54:25 +0800

    新增了公众号自动回复功能

--- WXPublic/views.py
+++ WXPublic/views.py
@@ -3,8 +3,48 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+from xml.etree import ElementTree
+
+import time
+
 import hashlib
 
+class ParseXMLMsg():
+    def __init__(self, xml_msg):
+        self.ToUserName = xml_msg.find('ToUserName').text
+        self.FromUserName = xml_msg.find('FromUserName').text
+        self.CreateTime = xml_msg.find('CreateTime').text
+        self.MsgType = xml_msg.find('MsgType').text
+        self.MsgId = xml_msg.find('MsgId').text
+        self.Content = xml_msg.find('Content').text
+        # self.ToUserName = xml_msg.find('ToUserName')
+        # self.FromUserName = xml_msg.find('FromUserName')
+        # self.CreateTime = xml_msg.find('CreateTime')
+        # self.MsgType = xml_msg.find('MsgType')
+        # self.MsgId = xml_msg.find('MsgId')
+        # self.Content = xml_msg.find('Content').encode('UTF-8')
+
+
+class SendMsg():
+    def __init__(self, toUserName, fromUserName, content):
+        self.__dict = dict()
+        self.__dict['ToUserName'] = toUserName
+        self.__dict['FromUserName'] = fromUserName
+        self.__dict['CreateTime'] = int(time.time())
+        self.__dict['Content'] = content
+
+    def send(self):
+        XmlForm = """
+            <xml>
+                <ToUserName><![CDATA[{ToUserName}]]></ToUserName>
+                <FromUserName><![CDATA[{FromUserName}]]></FromUserName>
+                <CreateTime>{CreateTime}</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[{Content}]]></Content>
+            </xml>
+            """
+        return XmlForm.format(**self.__dict)
+        
 
 def wx(request):
     # 用于验证微信服务器
@@ -25,4 +65,22 @@ def wx(request):
         else:
             return HttpResponse(content="验证失败")
     elif request.method == "POST":
-        pass
+        web_data = request.body
+        print(web_data)
+        xml_data = ElementTree.fromstring(web_data)
+        # 这里需要一个处理用户请求的函数
+        msg_from_user = ParseXMLMsg(xml_data)
+        # 转存用户请求  优先级往后排
+        
+        # 这里需要一个调用chat-gpt接口的函数
+        
+        # 转存回答  优先级往后排
+        
+        # 定时功能，超出4.5s后先给用户返回消息 优先级往后排
+        
+        # 这里需要一个返回用户消息的函数
+        content = ('你发送了这些内容：' + msg_from_user.Content).encode('UTF-8')
+        to_user = msg_from_user.FromUserName
+        from_user = msg_from_user.ToUserName
+        send_msg = SendMsg(to_user, from_user, content)
+        return HttpResponse(content=send_msg.send())
